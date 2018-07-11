1aa6e2f47373afbd0a661753d66a5c3b8e1af983
Berkat Bhatti <beme@theknightmarket.com>
2018-07-11 13:47:26 -0400

    Home view ui integrated

---
+++ Youtube/HomeVC.swift
@@ -0,0 +1,40 @@
+//
+//  HomeVC.swift
+//  Youtube
+//
+//  Created by berkat bhatti on 7/11/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        navigationItem.title = "Home"
+
+    }
+///protocol functions
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "videoCell", for: indexPath) as? videoCell else {return UICollectionViewCell()}
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+
+}
---
+++ Youtube/view/videoCell.swift
@@ -0,0 +1,13 @@
+//
+//  videoCell.swift
+//  Youtube
+//
+//  Created by berkat bhatti on 7/11/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+import UIKit
+
+class videoCell: UICollectionViewCell {
+    
+}
